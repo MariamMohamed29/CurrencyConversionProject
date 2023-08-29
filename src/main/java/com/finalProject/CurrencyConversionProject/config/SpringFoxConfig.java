42a687be61b6977ede78c805adbf932752e502e2
Mohamed Abdeldayem <142317163+mohamedabdeldayem7@users.noreply.github.com>
2023-08-29 17:00:56 +0300

    Update SpringFoxConfig.java

--- src/main/java/com/finalProject/CurrencyConversionProject/config/SpringFoxConfig.java
+++ src/main/java/com/finalProject/CurrencyConversionProject/config/SpringFoxConfig.java
@@ -17,7 +17,7 @@ public class SpringFoxConfig {
        return new Docket(DocumentationType.SWAGGER_2)
                .select()
 //                .apis(RequestHandlerSelectors.any())
-               .apis(RequestHandlerSelectors.basePackage("com.finalProject.CurrencyConversionProject.web.controller"))
+               .apis(RequestHandlerSelectors.basePackage("com/finalProject/CurrencyConversionProject/CurrencyConversionProjectApplication.java"))
                .paths(PathSelectors.any())
                .build()
                .apiInfo(metaData());
